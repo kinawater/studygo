c4e1da2be81eafd81078ae404fe60cbe72283163
kina lee <272553928@qq.com>
2021-12-01 10:31:00 +0800

    并发测试

--- goroutineTest3.go
+++ goroutineTest3.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main()  {
+	upc := make(chan string)
+	dwc := make(chan string)
+	go sourceGopher(upc)
+	go filterGopher(upc,dwc)
+	printGopher(dwc)
+}
+func sourceGopher(downstream chan string) {
+	for _, v:= range [] string{"hello oked","hi nope","moximoxi oked"}{
+		downstream <- v
+	}
+	close(downstream)
+}
+/**
+过滤不包含oked，并塞入新通道
+ */
+func filterGopher(upstream,downstream chan string) {
+	for item := range upstream {
+		if !strings.Contains(item,"oked") {
+			downstream <- item
+		}
+	}
+	close(downstream)
+}
+func printGopher(upstream chan string) {
+	for item:=range upstream{
+		fmt.Println(item)
+	}
+}
